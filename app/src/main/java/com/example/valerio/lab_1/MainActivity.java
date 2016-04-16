42b5eaee288e5fbaaa626a39b72d13274841ef65
Valerio <valerio.felici@itispaleocapa.it>
2016-04-16 17:29:37 +0200

    aggiunti radio button e log per verificarne il funzionamento

--- app/src/main/java/com/example/valerio/lab_1/MainActivity.java
+++ app/src/main/java/com/example/valerio/lab_1/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.valerio.lab_1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int durata= Toast.LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Accedi(View view) {
-
-
 
         TextView u = (TextView) findViewById(R.id.u_inserito);
         TextView p = (TextView) findViewById(R.id.p_inserito);
@@ -28,9 +30,26 @@ public class MainActivity extends AppCompatActivity {
             p.setText(pw.getText());
             u.setText(un.getText());
         } else {
-            Toast.makeText(MainActivity.this,R.string.campi_vuoti, Toast.LENGTH_LONG).show();
-
+            Toast.makeText(MainActivity.this,R.string.campi_vuoti, durata).show();
         }
+    }
 
+    public void Durata(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_breve:
+                if (checked)
+                    durata = Toast.LENGTH_SHORT;
+                    Log.i("durata avviso ", "corta");
+                    break;
+            case R.id.radio_lungo:
+                if (checked)
+                    durata = Toast.LENGTH_LONG;
+                    Log.i("durata avviso ", "lunga");
+                    break;
+        }
     }
 }
